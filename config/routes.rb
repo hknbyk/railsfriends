d648eb26a53a8b69c1402883f5e3238e419348ff
HAKAN BIYIK <hakanbiyik91@gmail.com>
2020-12-04 17:12:58 +0100

    added friend model

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # get 'home/index'
   get 'home/about'
   root 'home#index'
+  resources :friends
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
